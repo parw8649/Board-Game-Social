7122487bc062c5d7f69cdbfc4f6d8d41593919ba
BorisMarin8004 <bmar8004@gmail.com>
2021-10-30 14:48:27 -0700

    Registering tables with admin

--- Backend/BoardGameSocialAPI/admin.py
+++ Backend/BoardGameSocialAPI/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Event)
+admin.site.register(Post)
+admin.site.register(Game)
+admin.site.register(UserToUser)
+admin.site.register(Message)
+admin.site.register(EventToUser)
+admin.site.register(Comment)
+admin.site.register(GameToUser)
+admin.site.register(HostedGame)
+admin.site.register(Review)
+admin.site.register(HostedGameToUser)
