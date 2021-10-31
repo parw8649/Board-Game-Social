da0ccb8e01b8cc845003869c0686fdec4ad59817
parw8649 <cparwatkar@csumb.edu>
2021-10-31 20:32:33 +0530

    wiring up data and layout mapping for login page

--- Android/app/src/main/java/com/example/boardgamesocial/SignUpActivity.java
+++ Android/app/src/main/java/com/example/boardgamesocial/SignUpActivity.java
@@ -2,6 +2,8 @@ package com.example.boardgamesocial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SignUpActivity extends AppCompatActivity {
@@ -10,5 +12,9 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+    }
+
+    public static Intent getIntent(Context context) {
+        return new Intent(context, SignUpActivity.class);
     }
 }
