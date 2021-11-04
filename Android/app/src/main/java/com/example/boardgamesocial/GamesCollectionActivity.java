181b84d4d47bc8563126b25deef9f548f2ab20dc
parw8649 <cparwatkar@csumb.edu>
2021-11-04 09:49:42 +0530

    Wiring up navbar to games activity

--- Android/app/src/main/java/com/example/boardgamesocial/GamesCollectionActivity.java
+++ Android/app/src/main/java/com/example/boardgamesocial/GamesCollectionActivity.java
@@ -1,8 +1,13 @@
 package com.example.boardgamesocial;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class GamesCollectionActivity extends AppCompatActivity {
 
@@ -10,5 +15,20 @@ public class GamesCollectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_games_collection);
+
+        Toolbar nav_bar = findViewById(R.id.nav_bar_games_collection);
+        nav_bar.setTitle("");
+        setSupportActionBar(nav_bar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.navbar_options, menu);
+        return true;
+    }
+
+    public static Intent getIntent(Context context) {
+        return new Intent(context, GamesCollectionActivity.class);
     }
 }
