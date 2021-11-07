591bcc72e82a0daae0a4b7e4790e9c86c365e2b9
BorisMarin8004 <bmar8004@gmail.com>
2021-11-07 15:48:53 -0800

    Fixed Logout

--- Backend/BoardGameSocialAPI/views.py
+++ Backend/BoardGameSocialAPI/views.py
@@ -23,6 +23,7 @@ def signUpView(request):
 @api_view(["GET"])
 def logout(request):
     try:
+        Token.objects.get(**{k: v for k, v in request.GET.items()}).delete()
         res = get_response({"status": "User logout successful"}, status=200)
     except (TypeError, ValueError, Token.DoesNotExist, FieldError) as e:
         res = get_response({"error": str(e)}, status=400)
