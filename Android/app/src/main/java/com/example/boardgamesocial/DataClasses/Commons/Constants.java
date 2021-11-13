65a37aabe2e34987bed6c9866a250361e95d6695
parw8649 <cparwatkar@csumb.edu>
2021-11-13 22:31:28 +0530

    Added shared preferences utility for user session management

---
+++ Android/app/src/main/java/com/example/boardgamesocial/DataClasses/Commons/Constants.java
@@ -0,0 +1,7 @@
+package com.example.boardgamesocial.DataClasses.Commons;
+
+public class Constants {
+
+    public static final String PREFS = "com.example.ecstasywebstore.prefs";
+    public static final String USER_TOKEN = "com.example.boardgamesocial.userToken";
+}
