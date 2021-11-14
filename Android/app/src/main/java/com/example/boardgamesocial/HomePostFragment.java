115be16ec12aed0dc451201594676340b8de2c7c
BorisMarin8004 <bmar8004@gmail.com>
2021-11-13 20:55:06 -0800

    Added LiveData for DataClass representation

--- Android/app/src/main/java/com/example/boardgamesocial/HomePostFragment.java
+++ Android/app/src/main/java/com/example/boardgamesocial/HomePostFragment.java
@@ -1,66 +1,83 @@
 package com.example.boardgamesocial;
 
+import static com.example.boardgamesocial.API.RetrofitClient.getObjectList;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.boardgamesocial.DataClasses.DataClass;
+import com.example.boardgamesocial.DataClasses.Post;
+import com.example.boardgamesocial.DataViews.Adapters.DataClsAdapter;
+import com.example.boardgamesocial.DataViews.Adapters.OnGetView;
+import com.example.boardgamesocial.DataViews.DataClsVM;
 import com.example.boardgamesocial.databinding.FragmentHomePostBinding;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.gson.JsonArray;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class HomePostFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     private FragmentHomePostBinding binding;
 
-    public HomePostFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * returns a new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomePostFragment newInstance(String param1, String param2) {
-        HomePostFragment fragment = new HomePostFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private ListView recyclerView;
+    private DataClsAdapter<Post> dataClsAdapter;
+    private DataClsVM dataClsVM;
+    private List<Post> posts;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                            Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
         binding = FragmentHomePostBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        recyclerView = view.findViewById(R.id.postFeed_recyclerView);
+        posts = new ArrayList<>();
+        dataClsVM = new DataClsVM(Post.class, new HashMap<>());
+        dataClsVM.getLiveData().observe(getViewLifecycleOwner(), res -> {
+            posts.addAll(getObjectList((JsonArray) res, Post.class));
+            dataClsAdapter.notifyDataSetChanged();
+        });
+
+        if (dataClsAdapter == null) {
+            dataClsAdapter = new DataClsAdapter<>(R.layout.item, getContext(), posts, (postView, object) -> {
+                TextView postUser = postView.findViewById(R.id.item_poster);
+                TextView postType = postView.findViewById(R.id.item_type);
+                TextView postBody = postView.findViewById(R.id.item_body);
+                if (postUser != null) {
+                    postUser.setText(object.getUserId());
+                }
+                if (postType != null) {
+                    postType.setText(object.getPostType());
+                }
+                if (postBody != null) {
+                    postBody.setText(object.getPostBody());
+                }
+            });
+            recyclerView.setAdapter(dataClsAdapter);
+        } else {
+            dataClsAdapter.notifyDataSetChanged();
+        }
     }
 
     @Override
