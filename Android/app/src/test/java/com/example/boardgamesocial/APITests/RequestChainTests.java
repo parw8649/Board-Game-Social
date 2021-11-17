756cc640463b205405449ae2894dfd082f4da793
BorisMarin8004 <bmar8004@gmail.com>
2021-11-16 17:15:03 -0800

    Added more chain requests tests

--- Android/app/src/test/java/com/example/boardgamesocial/APITests/RequestChainTests.java
+++ Android/app/src/test/java/com/example/boardgamesocial/APITests/RequestChainTests.java
@@ -8,11 +8,14 @@ import android.util.Log;
 
 import com.example.boardgamesocial.API.APIMode;
 import com.example.boardgamesocial.API.RetrofitClient;
+import com.example.boardgamesocial.DataClasses.Event;
+import com.example.boardgamesocial.DataClasses.Game;
 import com.example.boardgamesocial.DataClasses.Post;
 import com.example.boardgamesocial.DataClasses.Relationships.UserToUser;
 import com.example.boardgamesocial.DataClasses.Token;
 import com.example.boardgamesocial.DataClasses.User;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -20,15 +23,21 @@ import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
+import io.reactivex.Observable;
+import io.reactivex.Observer;
 import io.reactivex.Scheduler;
 import io.reactivex.functions.Action;
+import io.reactivex.functions.BiFunction;
 import io.reactivex.schedulers.Schedulers;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+
+import androidx.annotation.NonNull;
 
 
 public class RequestChainTests {
@@ -76,6 +85,51 @@ public class RequestChainTests {
                     }
                     assertFalse(jsonArray.isEmpty());
                 })
+                .test()
+                .assertNoErrors();
+    }
+
+    @Test
+    public void mergeGetCallChainTest() {
+        retrofitClient.getCall(User.class, new HashMap<>())
+                .mergeWith(retrofitClient.getCall(Post.class, new HashMap<>()))
+                .mergeWith(retrofitClient.getCall(Game.class, new HashMap<>()))
+                .mergeWith(retrofitClient.getCall(Event.class, new HashMap<>()))
+                .scan((cumulativeJsonArray, newJsonArray) -> {
+                    cumulativeJsonArray.addAll(newJsonArray);
+                    return cumulativeJsonArray;
+                })
+                .doOnNext(jsonArrayCombined -> {
+                    Log.i(TAG, String.format("mergeGetCallChainTest: %s", jsonArrayCombined));
+                })
+                .test()
+                .assertNoErrors();
+    }
+
+    @Test
+    public void populatePostsChainTest() {
+        Integer userId = 245;
+        retrofitClient.getCall(UserToUser.class, new HashMap<String, String>(){{
+                    put("userOneId", String.valueOf(userId));
+                }})
+                .flatMap(jsonArray -> {
+                    Observable<JsonArray> postsObservable = retrofitClient.getCall(Post.class, new HashMap<String, String>(){{
+                        put("private", "False");
+                    }});
+                    List<UserToUser> usersWithPrivateAccess = getObjectList(jsonArray, UserToUser.class);
+                    for (UserToUser userWithPrivateAccess : usersWithPrivateAccess) {
+                        postsObservable.mergeWith(retrofitClient.getCall(Post.class, new HashMap<String, String>(){{
+                            put("userId", String.valueOf(userWithPrivateAccess.getUserTwoId()));
+                            put("private", "True");
+                        }})).test().assertNoErrors();
+                    }
+                    return postsObservable;
+                })
+                .scan((cumulativeJsonArray, newJsonArray) -> {
+                    cumulativeJsonArray.addAll(newJsonArray);
+                    return cumulativeJsonArray;
+                })
+                .doOnNext(jsonArrayCombined -> Log.i(TAG, String.format("populatePostsChainTest: FINAL %s", jsonArrayCombined)))
                 .test()
                 .assertNoErrors();
     }
