5bfc7968cee24fb9f5fd22d94467c18cfb8a8ddc
BorisMarin8004 <bmar8004@gmail.com>
2021-11-23 16:59:22 -0800

    Empty abstract class for DataClsVC

---
+++ Android/app/src/main/java/com/example/boardgamesocial/DataViews/ViewContainers/DataClsVC.java
@@ -0,0 +1,6 @@
+package com.example.boardgamesocial.DataViews.ViewContainers;
+
+import com.example.boardgamesocial.DataClasses.DataClass;
+
+public abstract class DataClsVC<DC extends DataClass> {
+}
